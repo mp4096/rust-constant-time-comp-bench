3a592037e058a3650e4359c2365c5cc908eaa5af
Mikhail Pak <mikhail.pak@tum.de>
2017-05-26 22:41:38 +0200

    Move to the built-in bench implementation

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,11 @@
+#![feature(test)]
+
 extern crate constant_time_eq;
-extern crate chrono;
+extern crate test;
 
 
-use chrono::{DateTime, Local};
-use constant_time_eq::constant_time_eq;
-
-const NUM_TRIALS: usize = 50000;
-const VEC_LEN: usize = 1000;
-
-fn main() {
-    let mut time_imperative_specialised: Vec<usize> = Vec::with_capacity(NUM_TRIALS);
-    let mut time_imperative_generic: Vec<usize> = Vec::with_capacity(NUM_TRIALS);
-    let mut time_functional_generic: Vec<usize> = Vec::with_capacity(NUM_TRIALS);
-
-    for _ in 0..NUM_TRIALS {
-        for l in vec![VEC_LEN] {
-            let left: Vec<u8> = vec![0; l];
-            let mut right: Vec<u8> = vec![0; l];
-            right[0] = 1;
-
-            let tic = Local::now();
-            ctc_imperative_generic(&left, &right);
-            time_imperative_generic.push(count_ns_since_datetime(tic));
-
-            let tic = Local::now();
-            ctc_functional_generic(&left, &right);
-            time_functional_generic.push(count_ns_since_datetime(tic));
-
-            let tic = Local::now();
-            constant_time_eq(&left, &right);
-            time_imperative_specialised.push(count_ns_since_datetime(tic));
-        }
-    }
-
-    println!("Avg. imperative:       {} ns", mean(&time_imperative_generic));
-    println!("Avg. functional:       {} ns", mean(&time_functional_generic));
-    println!("Avg. constant_time_eq: {} ns", mean(&time_imperative_specialised));
-}
-
-fn ctc_imperative_generic<T: PartialEq>(a: &[T], b: &[T]) -> bool {
+#[inline(never)]
+pub fn ctc_imperative_generic<T: PartialEq>(a: &[T], b: &[T]) -> bool {
     if a.len() != b.len() {
         return false;
     }
@@ -50,7 +17,32 @@ fn ctc_imperative_generic<T: PartialEq>(a: &[T], b: &[T]) -> bool {
     res
 }
 
-fn ctc_functional_generic<T: PartialEq>(a: &[T], b: &[T]) -> bool {
+#[inline(never)]
+pub fn ctc_imperative_generic_no_bound_checking<T: PartialEq>(a: &[T], b: &[T]) -> bool {
+    if a.len() != b.len() {
+        return false;
+    }
+
+    let len = a.len();
+    let a = &a[..len];
+    let b = &b[..len];
+
+    let mut res = true;
+    for i in 0..a.len() {
+        res &= a[i].eq(&b[i]);
+    }
+    res
+}
+
+#[inline(never)]
+pub fn ctc_imperative_specialised_no_bound_checking(a: &[u8], b: &[u8]) -> bool {
+    use constant_time_eq::constant_time_eq;
+
+    constant_time_eq(a, b)
+}
+
+#[inline(never)]
+pub fn ctc_functional_generic<T: PartialEq>(a: &[T], b: &[T]) -> bool {
     if a.len() != b.len() {
         return false;
     }
@@ -58,11 +50,60 @@ fn ctc_functional_generic<T: PartialEq>(a: &[T], b: &[T]) -> bool {
     a.iter().zip(b.iter()).fold(true, |c, (x, y)| c & (x.eq(y)))
 }
 
-#[inline]
-fn count_ns_since_datetime(dt: DateTime<Local>) -> usize {
-    Local::now().signed_duration_since(dt).num_nanoseconds().unwrap_or(0) as usize
+#[inline(never)]
+pub fn ctc_functional_specialised(a: &[u8], b: &[u8]) -> bool {
+    if a.len() != b.len() {
+        return false;
+    }
+
+    a.iter().zip(b.iter()).fold(0, |c, (x, y)| c | (x ^ y)) == 0
 }
 
-fn mean(x: &[usize]) -> f64 {
-    (x.iter().sum::<usize>() as f64)/(x.len() as f64)
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    const INPUT_LEN: usize = 100_000;
+
+    #[bench]
+    fn bench_functional_generic(b: &mut Bencher) {
+        let l = test::black_box(vec![0; INPUT_LEN]);
+        let r = test::black_box(vec![0; INPUT_LEN]);
+
+        b.iter(|| ctc_functional_generic(&l, &r));
+    }
+
+    #[bench]
+    fn bench_functional_specialised(b: &mut Bencher) {
+        let l = test::black_box(vec![0; INPUT_LEN]);
+        let r = test::black_box(vec![0; INPUT_LEN]);
+
+        b.iter(|| ctc_functional_specialised(&l, &r));
+    }
+
+    #[bench]
+    fn bench_imperative_generic(b: &mut Bencher) {
+        let l = test::black_box(vec![0; INPUT_LEN]);
+        let r = test::black_box(vec![0; INPUT_LEN]);
+
+        b.iter(|| ctc_imperative_generic(&l, &r));
+    }
+
+    #[bench]
+    fn bench_imperative_generic_no_bound_checking(b: &mut Bencher) {
+        let l = test::black_box(vec![0; INPUT_LEN]);
+        let r = test::black_box(vec![0; INPUT_LEN]);
+
+        b.iter(|| ctc_imperative_generic_no_bound_checking(&l, &r));
+    }
+
+    #[bench]
+    fn bench_imperative_specialised_no_bound_checking(b: &mut Bencher) {
+        let l = test::black_box(vec![0; INPUT_LEN]);
+        let r = test::black_box(vec![0; INPUT_LEN]);
+
+        b.iter(|| ctc_imperative_specialised_no_bound_checking(&l, &r));
+    }
 }
